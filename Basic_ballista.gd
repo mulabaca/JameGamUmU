040461c381ff36d9265c00ec3e1f3be1ce426293
Huu Huynh <huuhuynh1999@gmail.com>
2023-12-22 00:45:08 -0700

    icicle can shoot at any enemy with metadata but do doesn't dissappear with gobbo

--- Basic_ballista.gd
+++ Basic_ballista.gd
@@ -21,13 +21,13 @@ func _physics_process(delta):
 #this checks once when something enters
 func _on_tower_body_entered(body):
 	#test_Mob is the node in test.gd for spawning mob
-	if "Test_Mob" in body.name:
+	if "Enemy" in str(body.get_meta("Type")):
 		var tempArray = []
 		currTargets = get_node("Tower").get_overlapping_bodies()
 		#this is to remove the tower from its area and to have it only
 		#shoot at Mobs and nothing else
 		for i in currTargets:
-			if "Mob" in i.name:
+			if i != self and "Enemy" in str(i.get_meta("Type")):
 				tempArray.append(i)
 		#this is due to at the beginning there are no mobs in range
 		var currTarget = null
@@ -43,11 +43,12 @@ func _on_tower_body_entered(body):
 		
 		
 func shoot():
-	var Temp_Icicle = icicle.instantiate()
-	Temp_Icicle.dmg = icicle_dmg
-	Temp_Icicle.target = curr
-	get_node("Icicle_Container").add_child(Temp_Icicle)
-	Temp_Icicle.global_position = $Aim.global_position
+	if curr != null:
+		var Temp_Icicle = icicle.instantiate()
+		Temp_Icicle.dmg = icicle_dmg
+		Temp_Icicle.target = curr
+		get_node("Icicle_Container").add_child(Temp_Icicle)
+		Temp_Icicle.global_position = $Aim.global_position
 		
 #this is the Tower connecting(signal) to the body which is the Basic_Ballista for exiting
 #this checks every second if something has exited
@@ -57,7 +58,6 @@ func _on_tower_body_exited(body):
 	print("current targets", currTargets)
 	if curr == body:
 		curr = null
-	print("curr: ", curr)
 
 func _on_timer_timeout():
 	shoot()
